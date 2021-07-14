ff42c2ce39ce4fc9c747e2d54cba41213bd035d2
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2021-07-13 20:55:28 -0700

    Update index.js

--- it122/index.js
+++ it122/index.js
@@ -8,7 +8,7 @@ http.createServer((req,res) => {
             break;
         case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About page: Hello! My name is Diego Cano. I am 21 years old');
+            res.end('About page: Hello! My name is Diego Cano, and Im from Venezuela. Ive lived in Seattle for two years. I am a college student at Seattle Central College in the IT department. I have experience working with people because Ive worked as a waiter, barista, and banquet server. Id like to obtain my ASS-T in programming and try to transfer to a four-year college or university. My goal is to work for a company in the It team or as a freelancer.');
             break;
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
